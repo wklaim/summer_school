fixed sign error, all functions working corretly, next implement 5-pt stencil

advection.py
@@ -2,33 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Function defining initial conditions
-def initialBell(x):
+def initialPosBell(x):
 	return np.where(x%1. < 0.5 , np.power( np.sin( 2*x*np.pi), 2), 0)
 
-def burger_FTCS(parameters, total_time):
+def initialNegBell(x):
+	return np.where(x%1. < 0.5 , -1 * np.power( np.sin( 2*x*np.pi), 2), 0)
+	
+def burger_FTCS(parameters, total_time, initial_cond):
 #Parameters: space, time, initial profile
     nx = parameters['nx']           #No. of space steps
     dx = 1./nx         #space steps (m)
     nt = parameters['nt']          #No. of time steps
     dt = total_time/nt          #Time step in seconds
        
-    x = np.linspace(0.0, 1.0, nx+1)
-    uadv = initialBell(x)
+    uadv = initial_cond
     uadv_new = uadv.copy()
     uadv_old = uadv.copy()
     
-    ucons = initialBell(x)
+    ucons = initial_cond
     ucons_new = ucons.copy()
     ucons_old = ucons.copy()   
     #FTCS
     #For advective form     
     for n in range(1,nt):
         for j in range(1,nx):
-            uadv_new[j] = uadv[j]*( 1 + 2*dt/dx * ( uadv[j+1] - uadv[j-1] ) )
+            uadv_new[j] = uadv[j]*( 1 - 0.5*dt/dx * ( uadv[j+1] - uadv[j-1] ) )
             
             #Apply bound conditions
             #Calculate u[0] by using the penultimate u as u[-1]
-            uadv_new[0] = uadv[0]*( 1 + 2*dt/dx * ( uadv[1] - uadv[nx-1] ) )
+            uadv_new[0] = uadv[0]*( 1 - 0.5*dt/dx * ( uadv[1] - uadv[nx-1] ) )
             
             #update u for new timestep
             uadv_old = uadv.copy() 
@@ -38,31 +40,30 @@ def burger_FTCS(parameters, total_time):
       #Conservative form      
     for n in range(1,nt):
         for j in range(1,nx):
-            ucons_new[j] = ucons[j]*( 1 + 0.25*dt/dx * ( ucons[j+1]**2 - ucons[j-1]**2 ) )
+            ucons_new[j] = ucons[j]*( 1 - 0.125*dt/dx * ( ucons[j+1]**2 - ucons[j-1]**2 ) )
             
             #Apply bound conditions
             #Calculate u[0] by using the penultimate u as u[-1]
-            ucons_new[0] = ucons[0]*( 1 + 0.25*dt/dx * ( ucons[1]**2 - ucons[nx-1]**2 ) )
+            ucons_new[0] = ucons[0]*( 1 - 0.125*dt/dx * ( ucons[1]**2 - ucons[nx-1]**2 ) )
             
             #update u for new timestep
             ucons_old = ucons.copy() 
             ucons = ucons_new.copy()
         
-    return x, uadv, ucons     
+    return uadv, ucons     
 
-def burger_FTBS(parameters, total_time):
+def burger_FTBS(parameters, total_time, initial_cond):
 #Parameters: space, time, initial profile
     nx = parameters['nx']           #No. of space steps
     dx = 1./nx         #space steps (m)
     nt = parameters['nt']          #No. of time steps
     dt = total_time/nt          #Time step in seconds
   
-    x = np.linspace(0.0, 1.0, nx+1)
-    uadv = initialBell(x)
+    uadv = initial_cond
     uadv_new = uadv.copy()
     uadv_old = uadv.copy()
      
-    ucons = initialBell(x)
+    ucons = initial_cond
     ucons_new = ucons.copy()
     ucons_old = ucons.copy()   
     
@@ -70,11 +71,11 @@ def burger_FTBS(parameters, total_time):
          
     for n in range(1,nt):
         for j in range(1,nx):
-            uadv_new[j] = uadv[j]*( 1 + 0.5*dt/dx * ( uadv[j]**2 - uadv[j-1]**2 ) )
+            uadv_new[j] = uadv[j]*( 1 - 0.5*dt/dx * ( uadv[j] - uadv[j-1] ) )
             
             #Apply bound conditions
             #Calculate u[0] by using the penultimate u as u[-1]
-            uadv_new[0] = uadv[0]*( 1 + 0.5*dt/dx * ( uadv[0]**2 - uadv[nx-1]**2 ) )
+            uadv_new[0] = uadv[0]*( 1 - 0.5*dt/dx * ( uadv[0] - uadv[nx-1] ) )
             
             #update u for new timestep
             uadv_old = uadv.copy() 
@@ -82,17 +83,17 @@ def burger_FTBS(parameters, total_time):
             
     for n in range(1,nt):
         for j in range(1,nx):
-            ucons_new[j] = ucons[j]*( 1 + dt/dx * ( ucons[j] - ucons[j-1] ) )
+            ucons_new[j] = ucons[j]*( 1 - 0.125*dt/dx * ( ucons[j]**2 - ucons[j-1]**2 ) )
             
             #Apply bound conditions
             #Calculate u[0] by using the penultimate u as u[-1]
-            ucons_new[0] = ucons[0]*( 1 + dt/dx * ( ucons[0] - ucons[nx-1] ) )
+            ucons_new[0] = ucons[0]*( 1 - 0.125*dt/dx * ( ucons[0]**2 - ucons[nx-1]**2 ) )
             
             #update u for new timestep
             ucons_old = ucons.copy() 
             ucons = ucons_new.copy()
             
-    return x, uadv, ucons
+    return uadv, ucons
 
 
 def burger_adv_FT_5points(parameters, total_time):
@@ -121,35 +122,41 @@ def burger_adv_FT_5points(parameters, total_time):
             u_old = u.copy() 
             u = u_new.copy()
             
-    return x, u
+    return u
 
 
+def main():
+    #Parameters
+    param = {'nx': 70, 'nt': int(1e4)}
 
-#Parameters
-param = {'nx': 100, 'nt': int(1e4)}
+    #Execute code
+    total_time = 0.40    
+    x = np.linspace(0.0, 1.0, param['nx']+1)
+    pos_cond = initialPosBell(x)
+    neg_cond = initialNegBell(x)
+    
+    u_posCSadv , u_posCScons=  burger_FTCS(param, total_time, pos_cond) 
+    u_posBSadv, u_posBScons= burger_FTBS(param, total_time, pos_cond)
+    
+    #u_negCSadv , u_negCScons=  burger_FTCS(param, total_time, neg_cond) 
+    #u_negBSadv, u_negBScons= burger_FTBS(param, total_time, neg_cond)
 
-'''
-nx = 100            #No. of space steps
-dx = 1./nx         #space steps (m)
-nt = 10000           #No. of time steps
-dt = total_time/nt          #Time step in seconds
-'''   
+    #plot results
+    plt.figure(figsize=(12,5))
 
-#Execute code
-total_time = 0.07
-x, u_CSadv , u_CScons=  burger_FTCS(param, total_time) 
-x, u_BSadv, u_BScons= burger_FTBS(param, total_time)
-
-#plot results
-plt.plot(x, u_CSadv, 'yellow', label='adv form, FTCS')
-plt.plot(x, u_CScons, 'green', label='cons form, FTCS')
-plt.plot(x, u_BSadv, 'red', label='adv form, FTBS')
-plt.plot(x, u_BScons, 'blue', label='cons form, FTBS')
-plt.legend()
-plt.ylabel('u (ms-1)')
-time = 'Total time = {} seconds'.format(total_time)
-plt.text(0.5,1,time)
-plt.show()
+    #plt.plot(x, u_posCSadv, 'yellow', label='adv form, FTCS')
+    plt.plot(x, u_posCScons, 'green', label='cons form, FTCS')
+    plt.plot(x, u_posBSadv, 'red', label='adv form, FTBS')
+    plt.plot(x, u_posBScons, 'blue', label='cons form, FTBS')
+    plt.plot(x, pos_cond, 'grey', linestyle = '--', label='initial condition')
+    plt.legend()
+    plt.ylabel('u (ms-1)')
+    time = 'Total time = {} seconds'.format(total_time)
+    plt.text(0.5,1,time)
+    plt.title('+ve u initial cond.')
+    plt.show()
+    
+main()
     
     
     